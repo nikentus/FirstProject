e53be8903a4fe0e3e3f3df9ffacf8ae750a1f5bc
unknown <nikentus@gmail.com>
2017-07-29 20:06:05 +0300

    Some message

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+             
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double area = a * b;
+            Console.WriteLine(area);
+
+        }
+    }
+}
